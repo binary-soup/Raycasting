1184c88b48052b20dfd14fbc40a9c98b27ff1321
binary_soup <apps@binarysoup.dev>
2024-01-24 23:32:15 -0500

    Upgrade to 4.2

--- maze.gd
+++ maze.gd
@@ -19,10 +19,10 @@ class Tile extends Resource:
 func _draw():
 	var bounds := get_used_rect()
 	
-	var top_left := bounds.position * cell_quadrant_size
-	var top_right := Vector2(bounds.end.x, bounds.position.y) * cell_quadrant_size
-	var bottom_left := Vector2(bounds.position.x, bounds.end.y) * cell_quadrant_size
-	var bottom_right := bounds.end * cell_quadrant_size
+	var top_left := bounds.position * rendering_quadrant_size
+	var top_right := Vector2(bounds.end.x, bounds.position.y) * rendering_quadrant_size
+	var bottom_left := Vector2(bounds.position.x, bounds.end.y) * rendering_quadrant_size
+	var bottom_right := bounds.end * rendering_quadrant_size
 	
 	draw_colored_polygon([top_left, top_right, bottom_right, bottom_left], floor_colour)
 
